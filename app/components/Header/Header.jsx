ea74ad3a3da3c5807ffe944d3b9dff6bbb677267
Isuu1 <jakub.babelek@gmail.com>
2024-09-13 11:47:25 +0100

    Added link to home page to logo

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import styles from "@/app/components/Header/Header.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Image
-        className={styles.header__logo}
-        src="/images/typescript-icon.svg"
-        fill
-        alt=""
-      />
+      <Link href="/">
+        <Image
+          className={styles.header__logo}
+          src="/images/typescript-icon.svg"
+          fill
+          alt=""
+        />
+      </Link>
       <h1>Typescript Tutorial for newbies by Jakub B.</h1>
     </header>
   );
