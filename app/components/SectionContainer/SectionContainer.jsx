645b7505cdcc9703b9e5c2b92dfcc15e966c426d
Isuu1 <jakub.babelek@gmail.com>
2024-09-13 09:46:05 +0100

    Added more styling to sections container and sections wrapper

--- app/components/SectionContainer/SectionContainer.jsx
+++ app/components/SectionContainer/SectionContainer.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "@/app/components/SectionContainer/SectionContainer.module.scss";
 import Link from "next/link";
 
 const SectionContainer = ({ title, section, children }) => {
+  const [isSectionCompleted, setIsSectionCompleted] = useState(false);
+
   return (
     <section className={styles.sectionContainer}>
-      <Link href={`/section-${section}`}>
-        <h3 className={styles.sectionContainer__title}>{title}</h3>
-        <h4>In this section</h4>
-        {children}
-      </Link>
-      <button className={styles.sectionContainer__button}>
-        Mark as completed
+      {isSectionCompleted && (
+        <div className={styles.sectionCompletedOverlay}>
+          <h2>You completed this section</h2>
+        </div>
+      )}
+      <div>
+        <Link href={`/section-${section}`}>
+          <h3 className={styles.sectionContainer__title}>{title}</h3>
+          <h4>In this section</h4>
+          {children}
+        </Link>
+      </div>
+      <button
+        className={styles.sectionContainer__button}
+        onClick={() => setIsSectionCompleted(!isSectionCompleted)}
+      >
+        {isSectionCompleted
+          ? "Mark as not completed"
+          : "Mark as completed"}
       </button>
-      <input type="checkbox" />
     </section>
   );
 };
